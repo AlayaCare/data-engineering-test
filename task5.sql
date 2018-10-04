b80092efbad7ca43692f00b3e238c9887b22c0a3
Quentin Stoyel <quentin.stoyel@mail.mcgill.ca>
2018-10-04 18:36:09 -0400

    TASK-5: sql to add new client and corrsponding profile

--- task5.sql
+++ task5.sql
@@ -1,20 +1,24 @@
+-- Better way of doing this with unique indices, can't quite get it to work here though
+SELECT max(id) FROM profiles;
+SELECT max(id), max(guid) FROM profiles;
+
 INSERT INTO clients (id, description, guid, update_time, create_time, idmasteraccount, geofence, phone, display_id, idstatus, idaccounttype, idprofile) VALUES (2525, 'Task5 client', 591530, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP , 1000, '','','','active',0, 9315);
 
 
 INSERT INTO profiles (id, contact_id, attribute_id, value, crypted)
-VALUES (117470, 9305, 3, 'Task5', 0),
-(117471, 9315, 4, 'client', 0),
-(117482, 9315, 5, 'Montreal', 0),
-(117474, 9315, 6, 'Mrs.', 0),
-(117477, 9315, 9, 'QC', 0),
-(117489, 9315, 11, 'F', 0),
-(117487, 9315, 26, 'task5@client.com', 0),
-(117472, 9315, 18, '555-555-5555', 0),
-(117485, 9315, 30, '123 Sherbrooke St', 0),
-(117483, 9315, 31, 'QC', 0),
-(117486, 9315, 32, 'A1B 2C3', 0),
-(117473, 9315, 2034, '1900-00-00', 0),
-(117490, 9315, 2138, 'En', 0);
+VALUES (117514, 9305, 3, 'Task5', 0),
+(117515, 9315, 4, 'client', 0),
+(117516, 9315, 5, 'Montreal', 0),
+(117517, 9315, 6, 'Mrs.', 0),
+(117518, 9315, 9, 'QC', 0),
+(117519, 9315, 11, 'F', 0),
+(117520, 9315, 26, 'task5@client.com', 0),
+(117521, 9315, 18, '555-555-5555', 0),
+(117522, 9315, 30, '123 Sherbrooke St', 0),
+(117523, 9315, 31, 'QC', 0),
+(117524, 9315, 32, 'A1B 2C3', 0),
+(117525, 9315, 2034, '1900-00-00', 0),
+(117526, 9315, 2138, 'En', 0);
 
 
 
