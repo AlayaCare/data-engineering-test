9b1a6b2e1a7d8df15e3b1d1401369cee297e28cb
Quentin Stoyel <quentin.stoyel@mail.mcgill.ca>
2018-10-04 17:44:54 -0400

    sql to join client and profiles tables

---
+++ task2.sql
@@ -0,0 +1,8 @@
+SELECT * FROM clients 
+LEFT JOIN profiles ON clients.idprofile = profiles.contact_id 
+UNION ALL
+SELECT * FROM clients  
+RIGHT JOIN profiles ON clients.idprofile = profiles.contact_id 
+-WHERE clients.idprofile IS NULL
+
+-- Using UNION ALL because MySQL does not support FULL OUTER JOIN
